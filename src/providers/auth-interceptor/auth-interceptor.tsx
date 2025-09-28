2c7db60d40bf5df45e7244ff300cb1aeeeba272f
jhopn <jhpdrnsantos2@gmail.com>
2025-09-28 15:48:39 -0300

    fix: alteração na logica do interceptor odo auth

--- src/providers/auth-interceptor/auth-interceptor.tsx
+++ src/providers/auth-interceptor/auth-interceptor.tsx
@@ -1,38 +1,40 @@
 "use client";
-import { useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import { apiClient } from '@/services';
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { apiClient } from "@/services";
 
 export function AuthInterceptor() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    useEffect(() => {
-        const requestInterceptor = apiClient.interceptors.request.use(
-            (config) => {
-                if (session?.accessToken) {
-                    config.headers.Authorization = `Bearer ${session.accessToken}`;
-                }
-                return config;
-            },
-            (error) => Promise.reject(error)
-        );
+  useEffect(() => {
+    const requestInterceptor = apiClient.interceptors.request.use(
+      (config) => {
+        config.headers = config.headers || {};
+        if (session?.accessToken) {
+          config.headers.Authorization = `Bearer ${session.accessToken}`;
+        } else {
+          delete config.headers.Authorization;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
-        const responseInterceptor = apiClient.interceptors.response.use(
-            (response) => response,
-            (error) => {
-                if (error.response?.status === 401) {
-                    console.error("Não autorizado. Redirecionando...");
-                    window.location.href = "/";
-                }
-                return Promise.reject(error);
-            }
-        );
+    const responseInterceptor = apiClient.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          window.location.href = "/";
+        }
+        return Promise.reject(error);
+      }
+    );
 
-        return () => {
-            apiClient.interceptors.request.eject(requestInterceptor);
-            apiClient.interceptors.response.eject(responseInterceptor);
-        };
-    }, [session]);
+    return () => {
+      apiClient.interceptors.request.eject(requestInterceptor);
+      apiClient.interceptors.response.eject(responseInterceptor);
+    };
+  }, [session]);
 
-    return null;
+  return null;
 }
