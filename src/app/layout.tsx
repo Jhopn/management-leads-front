6e055620efb57a173cb03128685a9c5a30b83a11
jhopn <jhpdrnsantos2@gmail.com>
2025-09-28 01:39:26 -0300

    feat: criação das rotas privadas com autenticação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,29 @@
-"use client";
-import "./globals.css";
+// app/layout.tsx
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Verifique se este caminho está correto
+import AuthProvider from "@/providers/auth-provider/auth-provider";
 import Header from "@/components/header/header";
-import Footer from "@/components/footer/footer"
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+import Footer from "@/components/footer/footer";
+import "./globals.css";
+import { cookies } from "next/headers";
+
+export const metadata = {
+  title: "Meu App",
+  description: "Descrição do meu app",
+};
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
 
   return (
-    <html>
+    <html lang="pt-br">
       <body>
-        <Header />
-        {children}
-        <Footer />
+        <AuthProvider session={session}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
