3f837fd5495429d8bc9b42e4153e8e0a8527e649
jhopn <jhpdrnsantos2@gmail.com>
2025-09-28 01:36:45 -0300

    feat: criação do cabeçalho e footer do site

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <footer className="bg-gray-800 text-white py-4 mt-8">
+        <footer className="bg-[#292929] text-white py-4 mt-8">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                 <p className="text-sm">&copy; {new Date().getFullYear()} l0gik. Todos os direitos reservados.</p>
             </div>
